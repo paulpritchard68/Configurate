b77ca24d29b7f75f27048a460a506b964c4e720e
ExpatPaul <paulpritchard68@gmail.com>
2013-03-04 21:40:05 +0100

    New script added to copy files

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# A simple script to copy the other scripts to their correct folders
+# You will need to run this as root
+
+# Vim
+cp vimrc ~/.vimrc
+cp filetype.vim ~/.vim
+cp mysyntax.vim ~/.vim
+
+# Bash
+cp bashrc ~/.bashrc
+cp bashrc /root/.bashrc 
+
+
